c637340c671617f43376a4c47c69d928ebb4769a
tuslete <97281155+tuslete@users.noreply.github.com>
2022-01-13 13:03:29 +0100

    Work on updating the current date and time

--- src/script.js
+++ src/script.js
@@ -1,5 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.geMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = days[date.getDay()];
+  return `${currentDay}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#current-temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#current-city");
@@ -10,6 +32,8 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind-value");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let city = "Trondheim";
