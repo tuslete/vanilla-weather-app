f4afa88ca3b5dd5a754558cd6c86d2d6fb8cde22
tuslete <97281155+tuslete@users.noreply.github.com>
2022-01-13 10:36:40 +0100

    Updated js for current temperature, humidity, wind, city and description
    City, temperature, humidity, wind speed and description are updating with the current API data

--- src/script.js
+++ src/script.js
@@ -1,2 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#current-temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity-value");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind-value");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let city = "Trondheim";
 let apiKey = "c65e5bd2032ca8ff197ae617ffad0a4b";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let apiUrlCity = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrlCity).then(displayTemperature);
