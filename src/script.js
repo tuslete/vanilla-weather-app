242685103b68512e7d14360fd2bf4f134e5579cf
tuslete <97281155+tuslete@users.noreply.github.com>
2022-01-14 14:29:20 +0100

    Updated the weather icon

--- src/script.js
+++ src/script.js
@@ -34,6 +34,12 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#current-date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#current-icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let city = "Trondheim";
