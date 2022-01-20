90e0bea6018bdb0b1a9c2289c7ab29f02334ac8a
tuslete <97281155+tuslete@users.noreply.github.com>
2022-01-20 10:44:53 +0100

    Display the forecast data using the API

--- src/script.js
+++ src/script.js
@@ -21,33 +21,48 @@ function formatDate(timestamp) {
   return `${currentDay}, ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Thu", "Fri", "Sat", "Sun"];
+
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `    
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `    
             
     
               <div class="col-2">
               <div class="day">
                 <img
-                  src="http://openweathermap.org/img/wn/10d@2x.png"
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt="Clear"
                   id="icon-list"
                   class="list-of-icons"
                 />
+               
                 <br />
-                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-date">${formatDay(
+                  forecastDay.dt
+                )}</div>
                 <br />
-                7°C
+                <div class= "temperature-following-day"> ${Math.round(
+                  forecastDay.temp.day
+                )} °C</div>
              
             </div>
             
           </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
